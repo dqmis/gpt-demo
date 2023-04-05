b368d822e0fb6145810bf0b1b6da20b5b635f272
dqmis <dom.seputis@gmail.com>
2023-04-05 15:19:44 +0000

    Added few changes

--- train.py
+++ train.py
@@ -1,90 +1,95 @@
 import torch
-
-from src.models.mini_gpt import MiniGPT
-from src.utils.data import get_batch
+import hydra
+from src.models import MiniGPT
+from torch.utils.data import DataLoader
 from src.utils.evaluation import estimate_loss
+from omegaconf import DictConfig
+
+from tqdm import tqdm
+
+from typing import List
+
+from src.data.preprocessing.tokenziner import MiniGPTTokenizer
+from src.data.dataset import get_dataset, ShakespeareTokensDatataset
 
 from matplotlib import pyplot as plt
 
 
-# hyperparameters
-batch_size = 128  # how many independent sequences will we process in parallel?
-block_size = 128  # what is the maximum context length for predictions?
-max_iters = 5000
-eval_interval = 500
-learning_rate = 3e-4
-device = "cuda" if torch.cuda.is_available() else "cpu"
-eval_iters = 200
-n_embd = 288
-n_layer = 6
-n_head = 6
-dropout = 0.2
-# ------------
+@hydra.main(version_base=None, config_path="runs", config_name="config")
+def train(raw_cfg: DictConfig):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
 
-torch.manual_seed(1337)
+    cfg = raw_cfg["run"]["run"]
 
-# wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
-with open("./data/tinyshakespeare.txt", "r", encoding="utf-8") as f:
-    text = f.read()
+    torch.manual_seed(1337)
 
-# here are all the unique characters that occur in this text
-chars = sorted(list(set(text)))
-vocab_size = len(chars)
-# create a mapping from characters to integers
-stoi = {ch: i for i, ch in enumerate(chars)}
-itos = {i: ch for i, ch in enumerate(chars)}
-encode = lambda s: [
-    stoi[c] for c in s
-]  # encoder: take a string, output a list of integers
-decode = lambda l: "".join(
-    [itos[i] for i in l]
-)  # decoder: take a list of integers, output a string
+    # loading and splitting data into train and validation
+    raw_data = get_dataset("./data/tinyshakespeare.txt")
 
-# Train and test splits
-data = torch.tensor(encode(text), dtype=torch.long)
-n = int(0.9 * len(data))  # first 90% will be train, rest val
-train_data = data[:n]
-val_data = data[n:]
+    # cropping part of data for testing purposes
+    raw_data = raw_data[:1000]
+
+    # initializing and fitting the tokenizer
+    tokenizer = MiniGPTTokenizer()
+    tokenizer.fit(raw_data)
+
+    n = int(0.9 * len(raw_data))  # first 90% will be train, rest val
+    train_data = raw_data[:n]
+    val_data = raw_data[n:]
+
+    # initializing train and validation datasets
+    train_dataset = ShakespeareTokensDatataset(tokenizer, train_data, cfg.block_size)
+    val_dataset = ShakespeareTokensDatataset(tokenizer, val_data, cfg.block_size)
+
+    # creating dataloaders
+    train_dataloader = DataLoader(train_dataset, batch_size=cfg.batch_size, shuffle=True)
+    val_dataloader = DataLoader(val_dataset, batch_size=cfg.batch_size, shuffle=False)
+
+    # initializing the model
+    model = MiniGPT(
+        tokenizer.vocab_size, cfg.block_size, cfg.n_embd, cfg.n_head, cfg.n_layer
+    )
+    model = model.to(device)
+
+    print(sum(p.numel() for p in model.parameters()), "parameters")
+
+    # TRAINIGN LOOP
+    # create a PyTorch optimizer
+    optimizer = torch.optim.AdamW(model.parameters(), lr=cfg.learning_rate)
+
+    train_losses = []
+    val_losses = []
+
+    for iter in range(cfg.epochs):
+        # sample a batch of data
+        iter_train_losses: List[float] = []
+        for batch in tqdm(train_dataloader):
+            xb, yb = batch
+            xb = xb.to(device)
+            yb = yb.to(device)
+            # evaluate the loss
+            _, loss = model(xb, yb)
+            optimizer.zero_grad(set_to_none=True)
+            loss.backward()
+            optimizer.step()
+            iter_train_losses.append(loss.item())
+        val_loss = estimate_loss(model, val_dataloader, device)
+        train_loss = sum(iter_train_losses) / len(iter_train_losses)
+        train_losses.append(train_loss)
+        val_losses.append(val_loss)
+        print("train_loss: ", train_loss)
+        print("val_loss: ", val_loss)
+
+    # generate from the model
+    context = torch.zeros((1, 1), dtype=torch.long, device=device)
+    print(tokenizer.decode(model.generate(context, max_new_tokens=100)[0].tolist()))
+
+    plt.figure()
+    plt.plot(train_losses, label="train")
+    plt.plot(val_losses, label="val")
+    plt.legend()
+    plt.savefig("out.png")
 
 
-model = MiniGPT(vocab_size, block_size, n_embd, n_head, n_layer)
-m = model.to(device)
-
-train_losses = []
-val_losses = []
-
-print(sum(p.numel() for p in m.parameters()), "parameters")
-
-# create a PyTorch optimizer
-optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
-
-for iter in range(max_iters):
-    # every once in a while evaluate the loss on train and val sets
-    if iter % eval_interval == 0 or iter == max_iters - 1:
-        losses = estimate_loss(
-            m, eval_iters, train_data, val_data, block_size, device, batch_size
-        )
-        print(
-            f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}"
-        )
-        train_losses.append(losses["train"])
-        val_losses.append(losses["val"])
-
-    # sample a batch of data
-    xb, yb = get_batch("train", train_data, val_data, block_size, device, batch_size)
-
-    # evaluate the loss
-    logits, loss = model(xb, yb)
-    optimizer.zero_grad(set_to_none=True)
-    loss.backward()
-    optimizer.step()
-
-# generate from the model
-context = torch.zeros((1, 1), dtype=torch.long, device=device)
-print(decode(m.generate(context, max_new_tokens=100)[0].tolist()))
-
-plt.figure()
-plt.plot(train_losses, label="train")
-plt.plot(val_losses, label="val")
-plt.legend()
-plt.savefig("out.png")
+if __name__ == "__main__":
+    train()
